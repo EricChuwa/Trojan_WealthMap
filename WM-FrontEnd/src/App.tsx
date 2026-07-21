9803fd731ac173225e9c46ea28c46886f9a025e4
elijahkabatsi <e.kabatsi@alustudent.com>
2026-07-22 12:37:05 +1400

    Add Smell Test page and merge Investment Roadmap modal UI with Rwanda data

--- WM-FrontEnd/src/App.tsx
+++ WM-FrontEnd/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -12,6 +12,7 @@ import SplitScreen from "./pages/SplitScreen";
 import Learn from "./pages/Learn";
 import ScamAwareness from "./pages/ScamAwareness";
 import Goals from "./pages/Goals";
+import SmellTest from "./pages/SmellTest";
 import "./index.css";
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
         <Route path="/learn" element={<Learn />} />
         <Route path="/scams" element={<ScamAwareness />} />
         <Route path="/goals" element={<Goals />} />
+        <Route path="/smell-test" element={<SmellTest />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
